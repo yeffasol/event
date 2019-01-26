2a0b8865c5e2b91f66f5d3d3af78f6039d5c0bde
Marat <yeffasol@mail.ru>
2019-01-26 11:41:10 +0300

    Add es lint

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+	printWidth: 100,
+	singleQuote: false,
+	trailingComma: "all",
+	bracketSpacing: true,
+	tabWidth: 4,
+	useTabs: true,
+	semi: true,
+};
--- src/js/script.js
+++ src/js/script.js
@@ -1,46 +1,61 @@
 function addPhoneMask(elements) {
-	var phones = document.querySelectorAll(elements);
-	Array.prototype.forEach.call(phones, function (child) {
-		var mask = new IMask(child, {
-			mask: '{8}(000)000-00-00'
+	const phones = document.querySelectorAll(elements);
+	Array.prototype.forEach.call(phones, child => {
+		const mask = new IMask(child, {
+			mask: "{8}(000)000-00-00",
 		});
 	});
 }
 
 function addVoidForLinks(links) {
-	$.each(links, function () {
+	$.each(links, function() {
 		if ($(this).attr("href") == "" || $(this).attr("href") == "#") {
 			$(this).attr("href", "javascript:void(0)");
 		}
 	});
 }
 
-//a11y hamburger https://foxland.fi/simple-accessible-svg-menu-hamburger-animation
+// a11y hamburger https://foxland.fi/simple-accessible-svg-menu-hamburger-animation
 function hamburger(element, menu) {
-	var button = document.getElementById(element),
-		menu = document.getElementById(menu);
-	button.onclick = function () {
+	const button = document.getElementById(element);
+
+	var menu = document.getElementById(menu);
+	button.onclick = function() {
 		// Toggle class "opened". Set also aria-expanded to true or false.
-		if (-1 !== button.className.indexOf('opened')) {
-			button.className = button.className.replace(' opened', '');
-			button.setAttribute('aria-expanded', 'false');
-			menu.className = menu.className.replace(' active', '');
-			menu.setAttribute('aria-expanded', 'false');
+		if (button.className.indexOf("opened") !== -1) {
+			button.className = button.className.replace(" opened", "");
+			button.setAttribute("aria-expanded", "false");
+			menu.className = menu.className.replace(" active", "");
+			menu.setAttribute("aria-expanded", "false");
 		} else {
-			button.className += ' opened';
-			button.setAttribute('aria-expanded', 'true');
-			menu.className += ' active';
-			menu.setAttribute('aria-expanded', 'true');
+			button.className += " opened";
+			button.setAttribute("aria-expanded", "true");
+			menu.className += " active";
+			menu.setAttribute("aria-expanded", "true");
 		}
 	};
 }
 
-const doc = document,
-	root = document.getElementsByTagName('html')[0];
+const doc = document;
+
+const root = document.getElementsByTagName("html")[0];
 
 root.classList.remove("no-js");
+const person = {
+	name: "Yoda",
+	designation: "Jedi Master ",
+};
 
-//lazy load script file
+function trainJedi(jediWarrion) {
+	if (jediWarrion.name === "Yoda") {
+		console.log("No need! already trained");
+	}
+	console.log(`Training ${jediWarrion.name} complete`);
+}
+
+trainJedi(person);
+trainJedi({ name: "Adeel", designation: "padawan" });
+// lazy load script file
 // (function () {
 //     function component() {
 //         var element = document.createElement('div');
